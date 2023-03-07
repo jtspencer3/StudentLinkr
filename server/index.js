fd154f363ca476431140728cdaff4fcd7fcb7b84
Joel Spencer <jtspencer3@hotmail.com>
2023-03-07 15:25:06 -0600

    Changed port to 3001

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 // Port Number
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 // Creating express object
 const app = express();
