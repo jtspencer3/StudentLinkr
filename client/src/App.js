823d9cdebbff9ccb940fd02b2601678f3c9e6001
Joel Spencer <jtspencer3@hotmail.com>
2023-03-15 00:19:14 -0500

    Added page routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./Navigation/Navbar.js";
+// import Navbar from "./Navigation/Navbar.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import { Link } from 'react-router-dom';
 
 function App() {
  return(
   <div>
-    <p>Hello</p>
-    <Navbar/>
-    </div>
+     <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="about" element={ <About/> } />
+          <Route path="contact" element={ <Contact/> } />
+      </Routes>
+  </div>
  )
   /*const [data, setData] = React.useState(null);
 
