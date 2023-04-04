3d497ca673e6c2285bc0323cca163b035fce26eb
TCalhoun45 <tanner.renick53@icloud.com>
2023-04-04 00:07:51 -0500

    fixed navRight

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ const friends = [
   { photo: logo, name: "Jill Moncrief" },
   { photo: logo, name: "Jill Moncrief" },
   { photo: logo, name: "Jill Moncrief" },
+  { photo: logo, name: "Jill Moncrief" },
 ];
 
 function App() {
@@ -33,28 +34,19 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <div className="body"> */}
-      <div class="x9f619 x1n2onr6 x1ja2u2z">
-        <div class="x78zum5 xdt5ytf x1n2onr6 x1ja2u2z">
-          <div class="x78zum5 xdt5ytf x1n2onr6">
-            <div class="x78zum5 xdt5ytf x1n2onr6 xat3117 xxzkxad">
-              <div class="x78zum5 xdt5ytf x10cihs4 x1t2pt76 x1n2onr6 x1ja2u2z">
-                <div class="x9f619 x1n2onr6 x1ja2u2z x78zum5 x2lah0s xl56j7k x1qjc9v5 xozqiw3 x1q0g3np x1t2pt76 x17upfok">
-                  <div class="x9f619 x1n2onr6 x1ja2u2z x78zum5 x1r8uery x1iyjqo2 xs83m0k xeuugli x1qughib x1cy8zhl xozqiw3 x1q0g3np xylbxtu x1t2pt76 xornbnt">
+      
+            <div className="body">
+              <div className="content">
                     <NavRight items={friends} />
                     {/* <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
         </Routes> */}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                
+         </div>
+       </div>
+     </div>
   );
 }
 
