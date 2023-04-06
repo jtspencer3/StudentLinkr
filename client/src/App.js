c7680fc77e97db37028c2afd800124c91488946d
Joel Spencer <jtspencer3@hotmail.com>
2023-04-05 19:04:39 -0500

    Added redirect on successful login or registration

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import { Link } from "react-router-dom";
 import ProfileNavLeft from "./components/ProfileNavLeft.js";
 
@@ -57,17 +59,17 @@ const friends = [
   { photo: logo, name: "Jill Moncrief" },
 ];
 const pages = [
-  {name: "Home", link:"/"},
-  {name: "Groups", link:"/About"},
-  {name: "Discover", link:"/"},
-  {name: "Events", link:"/Contact"},
+  { name: "Home", link: "/" },
+  { name: "Groups", link: "/About" },
+  { name: "Discover", link: "/" },
+  { name: "Events", link: "/Contact" },
 ];
-const  pagesLeft = [
-  {photo:logo, name:"Tanner Renick", link:"/"},
-  {photo:logo, name:"Friends", link:"/About"},
-  {photo:logo, name:"Settings", link:"/About"},
-  {photo:logo, name:"Report A Bug", link:"/About"},
-  {photo:logo, name:"Logout", link:"/"},
+const pagesLeft = [
+  { photo: logo, name: "Tanner Renick", link: "/" },
+  { photo: logo, name: "Friends", link: "/About" },
+  { photo: logo, name: "Settings", link: "/About" },
+  { photo: logo, name: "Report A Bug", link: "/About" },
+  { photo: logo, name: "Logout", link: "/" },
   // {photo:logo, name:"Groups", link:"/About"},
   // {photo:logo, name:"Events", link:"/About"},
   // {photo:logo, name:"Settings", link:"/About"},
@@ -75,24 +77,84 @@ const  pagesLeft = [
   // {photo:logo, name:"Groups", link:"/About"},
   // {photo:logo, name:"Events", link:"/About"},
   // {photo:logo, name:"Settings", link:"/About"},
-]
+];
 const posts = [
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-  {name: "ZACH BODMER", dateTime:"DATETIME", post:"This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider."},
-]
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+  {
+    name: "ZACH BODMER",
+    dateTime: "DATETIME",
+    post: "This will be where the post content goes. I do not know if we should put a character limit on our posts or not but it is something we should consider.",
+  },
+];
 function App() {
   // const [data, setData] = React.useState(null);
 
@@ -104,19 +166,21 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar items ={pages} />
-            <div className="body">
-              <div className="content">
-                <ProfileNavLeft items ={pagesLeft}/>
-                     <Routes>
-          <Route path="/" element={<Home items ={posts} />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-        </Routes> 
-        <NavRight items={friends} />
-         </div>
-       </div>
-     </div>
+      <Navbar items={pages} />
+      <div className="body">
+        <div className="content">
+          <ProfileNavLeft items={friends} />
+          <Routes>
+            <Route path="/" element={<Home items={posts} />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Routes>
+          <NavRight items={friends} />
+        </div>
+      </div>
+    </div>
   );
 }
 
