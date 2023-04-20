2ce4f04af39d5bfc9615738b6be6442493ce7709
Joel Spencer <jtspencer3@hotmail.com>
2023-04-20 18:16:22 -0500

    Removed bcrypt import

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
-const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 //cookies
 const bodyParser = require("body-parser");
@@ -16,23 +15,27 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: ["http://localhost:3000"],
-  methods: ["GET", "POST"],
-  credentials: true
-}));
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(session({
-  key: "userID",
-  secret: "subscribe", //needs to be carful with this change laster
-  resave: "false",
-  saveUninitialized: "false",
-  cookie: {
-    expires: new Date(Date.now() + (30*24*3600000))   //This is how long cookies last 30 days
-  }
-}));
+app.use(
+  session({
+    key: "userID",
+    secret: "subscribe", //needs to be carful with this change laster
+    resave: "false",
+    saveUninitialized: "false",
+    cookie: {
+      expires: new Date(Date.now() + 30 * 24 * 3600000), //This is how long cookies last 30 days
+    },
+  })
+);
 // Database connection goes here
 // We can use dotenv and an .env file to store our database info privately
 // That .env file needs to be added to the git ignore and never pushed to github
@@ -47,14 +50,14 @@ app.post("/register", (req, res) => {
   const graduationYear = req.body.graduationYear;
   const password = req.body.password;
 
-  app.get("/login", (req, res) => {   //checks if user logged in
-    if (req.session.user){
-      res.send({loggedIn: true, user: req.session.user})
-    }else{
-      res.send({ loggedIn: false});
+  app.get("/login", (req, res) => {
+    //checks if user logged in
+    if (req.session.user) {
+      res.send({ loggedIn: true, user: req.session.user });
+    } else {
+      res.send({ loggedIn: false });
     }
   });
-
 
   const newUser = {
     first_name: firstName,
@@ -69,7 +72,7 @@ app.post("/register", (req, res) => {
 
   db.query("INSERT INTO users SET ?", newUser)
     .then((result) => {
-      console.log("After query")
+      console.log("After query");
       res.send({ message: "Success", redirect: "/login" });
       console.log("Inserted ${result.affectedRows} rows(s)");
     })
@@ -92,33 +95,30 @@ app.post("/login", (req, res) => {
 
   const login = {
     username: username,
-    password: password
+    password: password,
   };
   db.query("SELECT * FROM users WHERE username = ?", login.username)
-  .then((result) => {
-    console.log(login.password, " from react\n");
-    console.log(password, " from MySQL")
-    // if (result.length > 0) {
-    //   bcrypt.compare(login.password, result[0].password, (error, response) => {
-    //     if(response) {
-    //       req.session.user = result; //cookie always have user ID
-    //       console.log(req.session.user);
-    //       res.send({ message: "Success", redirect: "/", profile: result });
-    //     }else{
-    //       res.send({message : "Wrong username/password combination!"});
-    //     }
-    //   });
-    // } else{
-    //   res.send({ message: "User doesn't exist"});
-    //   }
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-        }
-      );
-
-    
+    .then((result) => {
+      console.log(login.password, " from react\n");
+      console.log(password, " from MySQL");
+      // if (result.length > 0) {
+      //   bcrypt.compare(login.password, result[0].password, (error, response) => {
+      //     if(response) {
+      //       req.session.user = result; //cookie always have user ID
+      //       console.log(req.session.user);
+      //       res.send({ message: "Success", redirect: "/", profile: result });
+      //     }else{
+      //       res.send({message : "Wrong username/password combination!"});
+      //     }
+      //   });
+      // } else{
+      //   res.send({ message: "User doesn't exist"});
+      //   }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // Server Setup
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
