ba0143276581a22f379163f881570063e593a9ed
Joel Spencer <jtspencer3@hotmail.com>
2023-05-02 18:55:14 -0500

    added new icons

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./App.css";
 import logo from "./logo.svg";
 import zach from "./Zach.jpg";
-import groupsIcon from "./GroupsIcon.svg";
-import errorIcon from "./erroricon.svg";
-import logoutIcon from "./logouticon.svg";
-import settingsIcon from "./settingsicon.svg";
+import groupsIcon from "./groupsIcon.png";
+import errorIcon from "./errorIcons.png";
+import logoutIcon from "./Logout icon.png";
+import settingsIcon from "./settingsIcon.png";
 import Navbar from "./components/Navbar.js";
 import NavRight from "./components/NavRight";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -167,17 +167,17 @@ function App() {
       <Navbar items={pages} />
       <div className="body">
         <div className="content">
-        <div className="content-seven">
-          <ProfileNavLeft items={pagesLeft} />
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="about" element={<About />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="logout" element={<Logout />} />
-          </Routes>
-          <NavRight items={friends} />
+          <div className="content-seven">
+            <ProfileNavLeft items={pagesLeft} />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+              <Route path="logout" element={<Logout />} />
+            </Routes>
+            <NavRight items={friends} />
           </div>
         </div>
       </div>
