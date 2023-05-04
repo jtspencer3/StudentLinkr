940ed84e62206ba56598d479316e365d0def664b
Joel Spencer <jtspencer3@hotmail.com>
2023-05-03 22:35:35 -0500

    removed unneccessary imports

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./../components/Home.css";
-import Post from "../components/Post";
-import Register from "./Register";
-import { Navbar } from "react-bootstrap";
 import NavRight from "../components/NavRight";
-import Login from "./Login";
 import Axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Feed from "../components/Feed";
@@ -146,7 +142,7 @@ function Home(props) {
 
   const loadHome = () => {
     const userID = userId; // change to dynamic based off cookie later
-    console.log(userId);
+
     //loads home feed posts based off of logged in user
     Axios.post("http://localhost:3001/getHome", {
       userID: userID,
@@ -159,12 +155,12 @@ function Home(props) {
     const userID = userId;
     console.log(userId);
     Axios.post("http://localhost:3001/getFollowing", {
-      userID: userID
+      userID: userID,
     }).then((response) => {
       var passResult = response.data.followingResults;
       setFollowingArray(passResult);
     });
-  }
+  };
   useEffect(() => {
     Axios.post("http://localhost:3001/checkSession").then((response) => {
       if (response.data.message === "loggedOut") {
@@ -182,24 +178,19 @@ function Home(props) {
 
   return (
     <div className="body">
-    <div className="content">
-      <div className="content-seven">
-        <ProfileNavLeft items={props.items} />
-        <div className="home">
-          {/* <div>
-        <form onSubmit={submitPost}>
-          <textarea type="input" id="postBox" ref={postRef}></textarea>
-          <button type="submit" id="postButton">
-            Post
-          </button>
-        </form>
-      </div> */}
-          <Feed items={postArray} />
+      <div className="content">
+        <div className="content-seven">
+          <ProfileNavLeft items={props.items} />
+          <div className="home">
+            <div>
+              <SubmitPost items={userId} />
+            </div>
+            <Feed items={postArray} />
+          </div>
+          <NavRight items={followingArray} />
         </div>
-        <NavRight items={followingArray} />
       </div>
     </div>
-  </div>
   );
 }
 
